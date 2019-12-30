2b2c3fcf207df6cea35600f7eb73d1c875372393
Larry850806 <pudding850806@gmail.com>
2019-12-30 18:25:39 +0800

    Rewrite with Rust

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+extern crate actix_rt;
+extern crate actix_web;
+extern crate enigo;
+
+use actix_files::NamedFile;
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, Result};
+
+use enigo::*;
+
+fn press(key: enigo::Key) {
+    let mut en = Enigo::new();
+    en.key_down(key);
+}
+
+async fn index(_req: HttpRequest) -> Result<NamedFile> {
+    Ok(NamedFile::open("index.html")?)
+}
+
+async fn press_space() -> impl Responder {
+    press(Key::Space);
+    "Ok"
+}
+
+async fn press_left() -> impl Responder {
+    press(Key::LeftArrow);
+    "Ok"
+}
+
+async fn press_right() -> impl Responder {
+    press(Key::RightArrow);
+    "Ok"
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/api/space", web::post().to(press_space))
+            .route("/api/left", web::post().to(press_left))
+            .route("/api/right", web::post().to(press_right))
+    })
+    .bind("0.0.0.0:3000")?
+    .run()
+    .await
+}
